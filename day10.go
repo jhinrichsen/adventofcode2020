6773146081db0e6ff606de6ccbaaed5d2582939f
Jochen <jot@localhost.localdomain>
2020-12-15 02:43:00 +0100

    feat: add day 10 part 2

--- day10.go
+++ day10.go
@@ -1,10 +1,12 @@
 package aoc2020
 
 import (
+	"fmt"
 	"sort"
 )
 
-func Day10(numbers []int) uint {
+// Day10Part1 returns number of 1 multiplied by number of 3 differences.
+func Day10Part1(numbers []int) uint {
 	sort.Ints(numbers)
 	var ones, threes uint
 	ones++   // count from 0 to first joltage
@@ -18,4 +20,51 @@ func Day10(numbers []int) uint {
 		}
 	}
 	return ones * threes
+}
+
+// combinations returns the number of different path for n consecutive numbers.
+// I figured out the values myself, looking up the sequence shows OIS sequence
+// A076739.
+func combinations(n int) uint {
+	switch n {
+	case 1:
+		return 1
+	case 2:
+		return 1
+	case 3:
+		return 2
+	case 4:
+		return 4
+	case 5:
+		return 7
+	default:
+		panic(fmt.Sprintf("unexpected range: %d", n))
+	}
+}
+
+// Day10Part2 returns number of combinations in O(n).
+func Day10Part2(numbers []int) uint {
+	n := uint(1)
+	// add a leading 0 and a trailing max+3
+	numbers = append(numbers, 0)
+	sort.Ints(numbers)
+	numbers = append(numbers, numbers[len(numbers)-1]+3)
+
+	nextGap := func(idx int) int {
+		for i := idx; i < len(numbers)-1; i++ {
+			delta := numbers[i+1] - numbers[i]
+			if delta == 3 {
+				return i + 1
+			} else if delta > 3 {
+				return i // backtrack to last index where delta < 3
+			}
+		}
+		return len(numbers)
+	}
+	for i := 0; i < len(numbers)-1; {
+		j := nextGap(i)
+		n *= combinations(j - i)
+		i = j
+	}
+	return n
 }
--- day10_test.go
+++ day10_test.go
@@ -1,7 +1,6 @@
 package aoc2020
 
 import (
-	"sort"
 	"testing"
 )
 
@@ -14,13 +13,19 @@ func testDay10(t *testing.T, filename string, part1 bool, want uint) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day10(numbers)
+	var f func([]int) uint
+	if part1 {
+		f = Day10Part1
+	} else {
+		f = Day10Part2
+	}
+	got := f(numbers)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
-func TestDay10Example1(t *testing.T) {
+func TestDay10Example1Part1(t *testing.T) {
 	const (
 		part1 = true
 		want  = 7 * 5
@@ -28,7 +33,7 @@ func TestDay10Example1(t *testing.T) {
 	testDay10(t, exampleFilename(10), part1, want)
 }
 
-func TestDay10Example2(t *testing.T) {
+func TestDay10Example2Part1(t *testing.T) {
 	const (
 		part1 = true
 		want  = 22 * 10
@@ -36,10 +41,48 @@ func TestDay10Example2(t *testing.T) {
 	testDay10(t, "testdata/day10_example2.txt", part1, want)
 }
 
-func TestDay10(t *testing.T) {
+func TestDay10Part1(t *testing.T) {
 	const (
 		part1 = true
 		want  = 1656
 	)
 	testDay10(t, filename(10), part1, want)
+}
+
+func TestDay10Example1Part2(t *testing.T) {
+	const (
+		part1 = false
+		want  = 8
+	)
+	testDay10(t, exampleFilename(10), part1, want)
+}
+
+func TestDay10Example2Part2(t *testing.T) {
+	const (
+		part1 = false
+		want  = 19208
+	)
+	testDay10(t, "testdata/day10_example2.txt", part1, want)
+}
+
+func TestDay10Part2(t *testing.T) {
+	const (
+		part1 = false
+		want  = 56693912375296
+	)
+	testDay10(t, filename(10), part1, want)
+}
+
+func BenchmarkDay10Part2IncludingInput(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lines, err := linesFromFilename(filename(10))
+		if err != nil {
+			b.Fatal(err)
+		}
+		numbers, err := linesAsNumbers(lines)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = Day10Part2(numbers)
+	}
 }
