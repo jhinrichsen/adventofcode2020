f6e02fdf6b1dc88b2300a9ab6e126f623141d59a
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-12-19 08:42:33 +0100

    feat: add day 15

--- day15.go
+++ day15.go
@@ -2,28 +2,44 @@ package aoc2020
 
 import "fmt"
 
-// Day15 returns the i-th number of the starting sequence.
-func Day15(numbers []uint, i int) uint {
-	// index is 1-based, transform to 0-based
-	i--
+// Day15 returns the n-th number of the starting sequence.
+func Day15(numbers []uint, idx1 int) uint {
+	// a number, and a list of its occurences as index
+	m := make(map[uint][]int)
 
-	idxBefore := func(i int) (int, error) { // search for highest index j < i, numbers[j] == numbers[i]
-		n := numbers[i-1]
-		for j := i - 2; j >= 0; j-- {
-			if numbers[j] == n {
-				return j, nil
-			}
-		}
-		return 0, fmt.Errorf("not found: %d", n)
-	}
-	for j := len(numbers); j <= i; j++ {
-		last, err := idxBefore(j)
-		if err != nil {
-			numbers = append(numbers, 0)
+	// store a number, and its index
+	store := func(n uint, i int) {
+		if ns, ok := m[n]; ok {
+			ns = append(ns, i)
+			m[n] = ns
 		} else {
-			lastLast, _ := idxBefore(last)
-			numbers = append(numbers, uint(lastLast-last))
+			m[n] = []int{i}
 		}
 	}
-	return numbers[i]
+
+	load := func(n uint) (int, int, error) {
+		// if ns, ok := m[n]; ok {
+		ns := m[n]
+		l := len(ns)
+		if l < 2 {
+			return 0, 0, fmt.Errorf("need %d but got %d", 2, len(ns))
+		}
+		return ns[l-1], ns[l-2], nil
+	}
+
+	for i := range numbers {
+		store(numbers[i], i)
+	}
+
+	var n uint
+	for i := len(numbers); i < idx1; i++ {
+		ultimate, penultimate, err := load(n)
+		if err == nil {
+			n = uint(ultimate - penultimate) // for deltas, we do not worry about 0 or 1 based indices
+		} else {
+			n = 0
+		}
+		store(n, i)
+	}
+	return n
 }
--- day15_test.go
+++ day15_test.go
@@ -18,6 +18,24 @@ var day15Samples = []struct {
 	{day15ExampleInput, 8, 0},
 	{day15ExampleInput, 9, 4},
 	{day15ExampleInput, 10, 0},
+	{day15ExampleInput, 2020, 436},
+
+	{[]uint{0, 3, 6}, 30000000, 175594},
+}
+
+// those take some bit of work
+var day15ExtendedSamples = []struct {
+	input []uint
+	i     int
+	want  uint
+}{
+	{[]uint{0, 3, 6}, 30000000, 175594}, // for benchmark, repeat first element
+	{[]uint{1, 3, 2}, 30000000, 2578},
+	{[]uint{2, 1, 3}, 30000000, 3544142},
+	{[]uint{1, 2, 3}, 30000000, 261214},
+	{[]uint{2, 3, 1}, 30000000, 6895259},
+	{[]uint{3, 2, 1}, 30000000, 18},
+	{[]uint{3, 1, 2}, 30000000, 362},
 }
 
 func TestDay15Examples(t *testing.T) {
@@ -27,6 +45,58 @@ func TestDay15Examples(t *testing.T) {
 			got := Day15(tt.input, tt.i)
 			if tt.want != got {
 				t.Fatalf("want %d but got %d", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestDay15ExtendedExamples(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping long running test cases")
+	}
+	for _, tt := range day15ExtendedSamples {
+		id := fmt.Sprintf("%d", tt.i)
+		t.Run(id, func(t *testing.T) {
+			got := Day15(tt.input, tt.i)
+			if tt.want != got {
+				t.Fatalf("want %d but got %d", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestDay15Part1(t *testing.T) {
+	const (
+		idx  = 2020
+		want = 1428
+	)
+	got := Day15([]uint{2, 0, 6, 12, 1, 3}, idx)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay15Part2(t *testing.T) {
+	const (
+		idx  = 30000000
+		want = 3718541
+	)
+	got := Day15([]uint{2, 0, 6, 12, 1, 3}, idx)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+// BenchmarkDay15 is a table driven benchmark.
+func BenchmarkDay15Part2(b *testing.B) {
+	if testing.Short() {
+		b.Skip("skipping long running benchmark")
+	}
+	for _, tb := range day15ExtendedSamples {
+		id := fmt.Sprintf("%+v", (tb.input))
+		b.Run(id, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = Day15(tb.input, tb.i)
 			}
 		})
 	}
