b1367ee48e2abeb0ed616d0774515c57f38acd46
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-12-20 13:08:40 +0100

    feat: add day18 part 2

--- day18.go
+++ day18.go
@@ -6,20 +6,31 @@ import (
 	"strings"
 )
 
-var opCfg = OperatorConfiguration{
+var part1Cfg = OperatorConfiguration{
 	"+": {1, false},
 	"*": {1, false},
 }
 
+var part2Cfg = OperatorConfiguration{
+	"+": {2, false},
+	"*": {1, false},
+}
+
 // Day18 calculates the result of each line, and returns the sum of all lines.
-func Day18(lines []string) (int, error) {
+func Day18(lines []string, part1 bool) (int, error) {
+	var cfg OperatorConfiguration
+	if part1 {
+		cfg = part1Cfg
+	} else {
+		cfg = part2Cfg
+	}
 	var sum int
 	for i, line := range lines {
 		// make sure parentheses have whitespace to the left and right
 		line = strings.ReplaceAll(line, "(", " ( ")
 		line = strings.ReplaceAll(line, ")", " ) ")
 
-		line = ShuntingYard(line, opCfg)
+		line = ShuntingYard(line, cfg)
 		n, err := eval(line)
 		if err != nil {
 			return 0, fmt.Errorf("line %d: %q results in %w",
--- day18_test.go
+++ day18_test.go
@@ -5,37 +5,48 @@ import (
 )
 
 var day18Examples = []struct {
-	in   string
-	want int
+	in        string
+	wantPart1 int
+	wantPart2 int
 }{
-	{"2 * 3 + ( 4 * 5 )", 26},
-	{"5 + ( 8 * 3 + 9 + 3 * 4 * 3 )", 437},
-	{"5 * 9 * ( 7 * 3 * 3 + 9 * 3 + ( 8 + 6 * 4 ) )", 12240},
-	{"( ( 2 + 4 * 9 ) * ( 6 + 9 * 8 + 6 ) + 6 ) + 2 + 4 * 2", 13632},
+	{"2 * 3 + ( 4 * 5 )", 26, 46},
+	{"5 + ( 8 * 3 + 9 + 3 * 4 * 3 )", 437, 1445},
+	{"5 * 9 * ( 7 * 3 * 3 + 9 * 3 + ( 8 + 6 * 4 ) )", 12240, 669060},
+	{"( ( 2 + 4 * 9 ) * ( 6 + 9 * 8 + 6 ) + 6 ) + 2 + 4 * 2", 13632, 23340},
 }
 
 func TestDay18Examples(t *testing.T) {
+	f := func(t *testing.T, part1 bool, infix string, want int) {
+		var cfg OperatorConfiguration
+		if part1 {
+			cfg = part1Cfg
+		} else {
+			cfg = part2Cfg
+		}
+		rpn := ShuntingYard(infix, cfg)
+		got, err := eval(rpn)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if want != got {
+			t.Fatalf("want %d but got %d", want, got)
+		}
+	}
 	for _, tt := range day18Examples {
 		id := tt.in
 		t.Run(id, func(t *testing.T) {
-			rpn := ShuntingYard(tt.in, opCfg)
-			got, err := eval(rpn)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if tt.want != got {
-				t.Fatalf("want %d but got %d", tt.want, got)
-			}
+			f(t, true, tt.in, tt.wantPart1)
+			f(t, false, tt.in, tt.wantPart2)
 		})
 	}
 }
 
-func testDay18(t *testing.T, filename string, want int) {
+func testDay18(t *testing.T, filename string, part1 bool, want int) {
 	lines, err := linesFromFilename(filename)
 	if err != nil {
 		t.Fatal(err)
 	}
-	got, err := Day18(lines)
+	got, err := Day18(lines, part1)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -48,12 +59,21 @@ func TestDay18Example(t *testing.T) {
 	const (
 		want = 26 + 437 + 12240 + 13632
 	)
-	testDay18(t, exampleFilename(18), want)
+	testDay18(t, exampleFilename(18), true, want)
 }
 
-func TestDay18(t *testing.T) {
+func TestDay18Part1(t *testing.T) {
 	const (
-		want = 21993583522852
+		part1 = true
+		want  = 21993583522852
 	)
-	testDay18(t, filename(18), want)
+	testDay18(t, filename(18), part1, want)
+}
+
+func TestDay18Part2(t *testing.T) {
+	const (
+		part1 = false
+		want  = 122438593522757
+	)
+	testDay18(t, filename(18), part1, want)
 }
