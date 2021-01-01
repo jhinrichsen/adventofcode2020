94841c7ddfea84c42f4b00915ab0a8e886bc4078
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2021-01-01 12:00:47 +0100

    feat: add day13 part 2 (incomplete)
    Implement a brute force solution, works for all examples but requires
    15s for last example. The real part #2 will take forever, so i need a
    CRT this time.

--- day13.go
+++ day13.go
@@ -28,3 +28,41 @@ func Day13(buf []byte, part1 bool) uint {
 	}
 	return minWait * minBusID
 }
+
+// Day13Part2BruteForce returns the point in time when all buses depart at offsets
+// matching their positions in the list.
+// Based on my 2016 day 15 solution.
+func Day13Part2BruteForce(input []int) uint {
+	// transform into functions where index is t and value is modulo
+	var fns []func(int) bool
+	for i := 0; i < len(input); i++ {
+		modulus := input[i] // remember Go kills iterator index in closures
+		fns = append(fns, func(t int) bool {
+			p := t%modulus == 0
+			return p
+		})
+	}
+
+	// return true if bus index n departs one minute before bus index n+1
+	nextMinute := func(idx, t int) bool {
+		p1 := fns[idx](t)
+		p2 := fns[idx+1](t + 1)
+		return p1 && p2
+	}
+
+	match := func(t uint) bool {
+		for j := 0; j < len(fns)-1; j++ {
+			p := nextMinute(j, int(t)+j)
+			if !p {
+				return false
+			}
+		}
+		return true
+	}
+
+	for t := uint(0); ; t++ {
+		if match(t) {
+			return t
+		}
+	}
+}
--- day13_test.go
+++ day13_test.go
@@ -1,7 +1,10 @@
 package aoc2020
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -33,22 +36,82 @@ func TestDay13(t *testing.T) {
 }
 
 func BenchmarkDay13Part1(b *testing.B) {
-	buf, err := ioutil.ReadFile(filename(13))
-	if err != nil {
-		b.Fatal(err)
-	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = Day13(buf, true)
-	}
-}
-
-func BenchmarkDay13Part1IncludingInput(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		buf, err := ioutil.ReadFile(filename(13))
 		if err != nil {
 			b.Fatal(err)
 		}
 		_ = Day13(buf, true)
+	}
+}
+
+const (
+	x = 1 // neutral modulo, n % 1 = 0
+)
+
+var day13Part2Examples = []struct {
+	input []int
+	want  uint
+}{
+	{[]int{67, 7, 59, 61}, 754018},
+	{[]int{7, 13, x, x, 59, x, 31, 19}, 1068781},
+	{[]int{17, x, 13, 19}, 3417},
+	{[]int{67, 7, 59, 61}, 754018},
+	{[]int{67, x, 7, 59, 61}, 779210},
+	{[]int{67, 7, x, 59, 61}, 1261476},
+	{[]int{1789, 37, 47, 1889}, 1202161486},
+}
+
+func TestDay13Part2Examples(t *testing.T) {
+	for _, tt := range day13Part2Examples {
+		id := fmt.Sprintf("%+v", tt.input)
+		t.Run(id, func(t *testing.T) {
+			// skip last example in short testing mode
+			if testing.Short() && tt.want > 10_000_000 {
+				msg := fmt.Sprintf("skipping long running %+v",
+					tt.input)
+				t.Skip(msg)
+			}
+
+			got := Day13Part2BruteForce(tt.input)
+			if tt.want != got {
+				t.Fatalf("want %d but got %d", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestDay13Part2(t *testing.T) {
+	const want = 13
+
+	if testing.Short() {
+		t.Skip("skipping part #2 because i can only do brute force")
+	}
+
+	lines, err := linesFromFilename(filename(13))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// convert to numbers
+	buses := strings.Split(lines[1], ",")
+	idxs := make([]int, len(buses))
+	for i := range buses {
+		var line int
+		if buses[i] == "x" {
+			line = 1
+		} else {
+			n, err := strconv.Atoi(buses[i])
+			if err != nil {
+				t.Fatalf("error parsing field %d: %+v", i, err)
+			}
+			line = n
+		}
+		idxs[i] = line
+	}
+
+	got := Day13Part2BruteForce(idxs)
+	if want != got {
+		t.Fatalf("want %d but got %d\n", want, got)
 	}
 }
